f3e47cd1f6d7d0299f19adc62563e550c79274a2
bbsjhsn <1754670086@qq.com>
2026-04-22 17:58:12 +0800

    Fix datastore remove import

--- app/src/main/java/com/customalarm/app/data/repository/AppSettingsRepository.kt
+++ app/src/main/java/com/customalarm/app/data/repository/AppSettingsRepository.kt
@@ -6,7 +6,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.core.remove
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.core.os.LocaleListCompat
